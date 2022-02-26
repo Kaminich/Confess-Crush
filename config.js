903b35e2c4e540b6cd66e6b114d55b4adabb95aa
Kaminich <100463988+Kaminich@users.noreply.github.com>
2022-02-26 15:52:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 const CONFIG = {
     titleWeb: "Tỏ tình online",
     introTitle: 'Tên người yêu bạn',
-    introDesc: `Tui thích bạn lắm\n
-    Làm người yêu tui nha`,
+    introDesc: `Tui thích bạn lắm, làm người yêu tui nha`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Okie la, tui cũng thích bạn lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn 🌹❤🌹❤',
-    reply: 'Tại bạn cute phô mai que quá',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình <3',
+    btnReply: 'Gửi cho bạn',
+    reply: 'Tại bạn cute phô mai que quá 😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘',
+    mess: 'Mình biết mà 🥰. Iu bạn nhiều nhiều 😘😘',
     messDesc: 'Vậy từ bây giờ mình là người iu nha💕.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/caominh.nguyen.338'
