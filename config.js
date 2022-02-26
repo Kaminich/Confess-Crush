a9ed687fb65197b1a366fc46447eed4e8a1ef20c
Kaminich <100463988+Kaminich@users.noreply.github.com>
2022-02-26 15:55:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-    titleWeb: "Tỏ tình online",
+    titleWeb: "Tỏ tình online <3",
     introTitle: 'Tên người yêu bạn',
     introDesc: `Tui thích bạn lắm, làm người yêu tui nha`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Okie la, tui cũng thích bạn lắm <33',
+    btnYes: 'Okie la <3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình <3',
     btnReply: 'Gửi cho bạn',
